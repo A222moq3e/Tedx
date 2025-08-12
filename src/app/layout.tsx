195a57cdfc7ff23d7e2066657db676ba175762a5
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2025-08-12 12:55:00 +0300

    Fixes theming hydration error https://ui.shadcn.com/docs/dark-mode/next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" suppressHydrationWarning className={`${geist.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
